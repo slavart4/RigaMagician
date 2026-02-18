955b93ee02afed49f8c96de557e91ddfa0cdeefb
slavart <slavart4@gmail.com>
2026-02-18 15:42:44 +0200

    add Attacks class

---
+++ src/Attacks.cpp
@@ -0,0 +1,82 @@
+#include "Attacks.h"
+
+namespace Rmagician {
+
+void Attacks::init() {
+    for (int s = 0; s < 64; ++s) {
+        Bitboard b = 1ULL << s;
+
+        knight_attacks_[s] =
+            BitboardUtils::shift(b, static_cast<Direction>(NORTH+NORTH_EAST)) |
+            BitboardUtils::shift(b, static_cast<Direction>(NORTH+NORTH_WEST)) |
+            BitboardUtils::shift(b, static_cast<Direction>(SOUTH+SOUTH_EAST)) |
+            BitboardUtils::shift(b, static_cast<Direction>(SOUTH+SOUTH_WEST)) |
+            BitboardUtils::shift(b, static_cast<Direction>(EAST+NORTH_EAST))  |
+            BitboardUtils::shift(b, static_cast<Direction>(EAST+SOUTH_EAST))  |
+            BitboardUtils::shift(b, static_cast<Direction>(WEST+NORTH_WEST))  |
+            BitboardUtils::shift(b, static_cast<Direction>(WEST+SOUTH_WEST));
+
+        king_attacks_[s] =
+            BitboardUtils::shift(b, NORTH) |
+            BitboardUtils::shift(b, SOUTH) |
+            BitboardUtils::shift(b, EAST)  |
+            BitboardUtils::shift(b, WEST)  |
+            BitboardUtils::shift(b, NORTH_EAST) |
+            BitboardUtils::shift(b, NORTH_WEST) |
+            BitboardUtils::shift(b, SOUTH_EAST) |
+            BitboardUtils::shift(b, SOUTH_WEST);
+    }
+}
+
+Bitboard Attacks::ray_attack(Square sq, Direction dir, Bitboard occupied) {
+    Bitboard attacks = 0ULL;
+    int rank = sq / 8;
+    int file = sq % 8;
+
+    int dr = (dir == NORTH || dir == NORTH_EAST || dir == NORTH_WEST) ? 1 :
+             (dir == SOUTH || dir == SOUTH_EAST || dir == SOUTH_WEST) ? -1 : 0;
+    int df = (dir == EAST || dir == NORTH_EAST || dir == SOUTH_EAST) ? 1 :
+             (dir == WEST || dir == NORTH_WEST || dir == SOUTH_WEST) ? -1 : 0;
+
+    int cur_rank = rank + dr;
+    int cur_file = file + df;
+
+    while (cur_rank >= 0 && cur_rank < 8 && cur_file >= 0 && cur_file < 8) {
+        Bitboard bit = 1ULL << (cur_rank * 8 + cur_file);
+        attacks |= bit;
+        if (occupied & bit) break;
+        cur_rank += dr;
+        cur_file += df;
+    }
+    return attacks;
+}
+
+Bitboard Attacks::get_rook_attacks(Square sq, Bitboard occupied) {
+    return
+        ray_attack(sq, NORTH, occupied) |
+        ray_attack(sq, SOUTH, occupied) |
+        ray_attack(sq, EAST, occupied)  |
+        ray_attack(sq, WEST, occupied);
+}
+
+Bitboard Attacks::get_bishop_attacks(Square sq, Bitboard occupied) {
+    return
+        ray_attack(sq, NORTH_EAST, occupied) |
+        ray_attack(sq, NORTH_WEST, occupied) |
+        ray_attack(sq, SOUTH_EAST, occupied) |
+        ray_attack(sq, SOUTH_WEST, occupied);
+}
+
+Bitboard Attacks::get_queen_attacks(Square sq, Bitboard occupied) {
+    return get_rook_attacks(sq, occupied) |
+           get_bishop_attacks(sq, occupied);
+}
+
+Bitboard Attacks::get_knight_attacks(Square sq) {
+    return knight_attacks_[sq];
+}
+Bitboard Attacks::get_king_attacks(Square sq) {
+    return king_attacks_[sq];
+}
+
+}
---
+++ src/Attacks.h
@@ -0,0 +1,98 @@
+#ifndef RIGAMAGICIAN_ATTACKS_H
+#define RIGAMAGICIAN_ATTACKS_H
+
+#include "BitboardUtils.h"
+
+/// TODO: for now we find pieces possible attacks "on fly"
+///  but better rewrite to magic bitmasks
+///  (precalculated attacks for all squares and pieces)
+
+namespace Rmagician {
+
+/*!
+ * @class Attacks
+ * @brief Attack generation helper for chess pieces.
+ */
+class Attacks {
+public:
+    /*!
+     * @brief Initializes precomputed attack tables.
+     *
+     * @details
+     * Must be called once before querying leaper attacks.
+     * Fills internal tables with attacks for all 64 squares.
+     */
+    void init();
+
+    /*!
+     * @brief Returns rook attacks from a square.
+     * @param sq Source square.
+     * @param occupied Bitboard of all occupied squares.
+     * @return Bitboard of attacked squares until first blocker
+     * in each rook direction.
+     */
+    Bitboard get_rook_attacks(Square sq, Bitboard occupied);
+
+    /*!
+     * @brief Returns bishop attacks from a square.
+     * @param sq Source square.
+     * @param occupied Bitboard of all occupied squares.
+     * @return Bitboard of attacked squares until first blocker
+     * in each bishop direction.
+     */
+    Bitboard get_bishop_attacks(Square sq, Bitboard occupied);
+
+    /*!
+     * @brief Returns queen attacks from a square.
+     * @param sq Source square.
+     * @param occupied Bitboard of all occupied squares.
+     * @return Union of rook and bishop attacks from the source square.
+     */
+    Bitboard get_queen_attacks(Square sq, Bitboard occupied);
+
+    /*!
+     * @brief Returns knight attacks from a square.
+     * @param sq Source square.
+     * @return Precomputed knight attack bitboard.
+     */
+    Bitboard get_knight_attacks(Square sq);
+
+    /*!
+     * @brief Returns kings attacks from a square.
+     * @param sq Source square.
+     * @return Precomputed king attack bitboard.
+     *
+     * @note Returns pseudo-legal king moves (can be moved on attacked square)
+     */
+    Bitboard get_king_attacks(Square sq);
+private:
+    /*!
+     * @brief Traces attacks in one direction until the edge or blocker.
+     * @param sq Source square.
+     * @param dir Ray direction.
+     * @param occupied Bitboard of occupied squares.
+     * @return Bitboard of squares attacked in some direction.
+     */
+    Bitboard ray_attack(Square sq, Direction dir, Bitboard occupied);
+
+    /*! Precomputed knight attacks for each square [0..63]. */
+    Bitboard knight_attacks_[64] = {};
+
+    /*! Precomputed king attacks for each square [0..63]. */
+    Bitboard king_attacks_[64] = {};
+
+    /*
+     * Harder to deal with bcs this pieces cannot jump over other pieces.
+     * So we have to deal with obstacles and store all possible variants.
+     * Maybe do it later, for now just calculate attack.
+     */
+    // Bitboard bishop_attacks_[64];
+    // Bitboard queen_attacks_[64];
+    // Bitboard rook_attacks_[64];
+    // Bitboard pawn_attacks_[COLOR_NUM][64];
+
+}; // class Attacks
+
+} // namespace Rmagician
+
+#endif //RIGAMAGICIAN_ATTACKS_H
